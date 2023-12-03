077d0cc2d84695d76fed616670eef314b14abe9f
Subrahmanya Mayya <subrahmanya803@gmail.com>
2023-12-03 19:31:46 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,116 @@
+import random
+from flask import Flask, request, jsonify
+import nltk
+import pickle
+import tflearn
+import json
+import numpy as np
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+nltk.download('punkt')
+app = Flask(__name__)
+
+# Example dictionary to store conversation data
+conversation_data = []
+
+train_x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+train_y = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+net = tflearn.input_data(shape=[None, len(train_x)])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y), activation='softmax')
+net = tflearn.regression(net)
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+print("Loading Pickle.....")
+data = pickle.load( open( "training_data", "rb" ) )
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+print("Loading the Model......")
+# load our saved model
+model.load('./model.tflearn')
+
+def clean_up_sentence(sentence):
+    # It Tokenize or Break it into the constituents parts of Sentense.
+    sentence_words = nltk.word_tokenize(sentence)
+    # Stemming means to find the root of the word.
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=False):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+ERROR_THRESHOLD = 0.25
+
+def classify(sentence):
+    # Prediction or To Get the Posibility or Probability from the Model
+    results = model.predict([bow(sentence, words)])[0]
+    # Exclude those results which are Below Threshold
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    # Sorting is Done because heigher Confidence Answer comes first.
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1])) #Tuppl -> Intent and Probability
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    # That Means if Classification is Done then Find the Matching Tag.
+    if results:
+        # Long Loop to get the Result.
+        while results:
+            for i in intents['intents']:
+                # Tag Finding
+                if i['tag'] == results[0][0]:
+                    # Random Response from High Order Probabilities
+                    return random.choice(i['responses'])
+
+            results.pop(0)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    if request.method == 'POST':
+        # Assuming the conversation data is sent as JSON in the request
+        data = request.json
+        
+        # Assuming the conversation data is structured as a dictionary with a 'message' key
+        if 'message' in data and data['message'].strip():  # Checking for non-empty string
+            message = data['message']
+            
+            # Append the received message to the conversation data list
+            conversation_data.append(message)
+            
+            res = response(message)
+            print(res)
+            
+            
+            return jsonify({'status': 'OK','response':res})
+        else:
+            return jsonify({'error': 'Empty message or message not found'}), 400  # Return a 400 Bad Request status
+
+if __name__ == '__main__':
+    app.run(debug=True)
