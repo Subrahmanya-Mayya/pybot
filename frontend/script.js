5923fdbbf5246105569e084c10b4e1532cc57b7a
Subrahmanya Mayya <subrahmanya803@gmail.com>
2023-12-03 22:31:56 +0530

    fix word wrap

--- frontend/script.js
+++ frontend/script.js
@@ -89,6 +89,3 @@ sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () =>
   document.body.classList.remove("show-chatbot")
 );
-chatbotToggler.addEventListener("click", () =>
-  document.body.classList.toggle("show-chatbot")
-);
